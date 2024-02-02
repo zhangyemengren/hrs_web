1a635fbca04e1bfc7341b44d040a468ef3b2d085
zhangyemengren <624593758@qq.com>
2024-02-02 19:18:55 +0800

    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
-    integrations: [react()]
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
+  integrations: [react(), tailwind()]
 });
