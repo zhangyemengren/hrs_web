945069ba129ef083c2b1c2e5dac899041dd6fd9f
zhangyemengren <624593758@qq.com>
2024-02-02 10:06:27 +0800

    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [react()]
+    output: "server",
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    integrations: [react()]
 });
--- src/react_components/Input.tsx
+++
@@ -1,8 +0,0 @@
-interface InputProps {
-    name?: string;
-    type?: string;
-}
-export default function (props:InputProps){
-    const {type = "text", name = ""} = props;
-    return <input type={type} name={name} />
-}
